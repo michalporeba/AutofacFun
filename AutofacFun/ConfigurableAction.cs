7ee1c02ed6893bed5bd1f7aae941fa6676e94b89
Michał Poręba <michalporeba@gmail.com>
2020-05-29 19:24:26 +0100

    exploring autofac configuration

---
+++ AutofacFun/ConfigurableAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AutofacFun
+{
+    public interface IConfigurableAction
+    {
+        string Execute();
+    }
+    
+    public class ConfigurableAction
+        : IConfigurableAction
+    {
+        private readonly Configuration _config;
+        
+        public ConfigurableAction(Configuration config)
+        {
+            _config = config;
+        }
+
+        public string Execute()
+        {
+            return $"Configurable Action - {_config.Value}";
+        }
+    }
+
+    public class Configuration
+    {
+        public string Value { get; set; }
+    }
+}
